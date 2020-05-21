6e11ec582568c7e432890070d66375fb8afeb914
Michael Schwarz <mschwarz@multitool.net>
2020-05-21 16:47:00 -0500

    Add some developer notes

--- src/mp4titler.pl
+++ src/mp4titler.pl
@@ -1,5 +1,19 @@
 #! /usr/bin/perl
 
+## Some dev notes:
+#
+# I like to use PAR::Packer to make perl scripts into self-contained binaries sometimes.
+# Before doing that, I would prbably want to use something like Getopt::Long to
+# parameterize the $pattern and the $mod setting, and to have if $mod is zero, no
+# run time output. I may do that in my non-existant apre time.
+#
+# This code is just public domain. There is nothing here that needs licensing. Do what
+# you like with it.
+#
+# Written by Michael Schwarz
+# mschwarz at multitool dot net
+
+
 use strict;
 
 use Image::ExifTool;
