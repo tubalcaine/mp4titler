f4d152533f5c9f69daa259c7b790dfb83ba12062
Michael Schwarz <mschwarz@multitool.net>
2020-05-21 16:35:33 -0500

    Initial commit of code and description.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # mp4titler
 A perl script that will set a media file's Title tag to match its base file name.
+
+I had a very large collection of video files created with various cameras, devices, and
+from various digital media sources. I used to access these primarily with a file server,
+either nfs for *nix machines, or SMB for Windows. I have a lot of DLNA compatible 
+devices throughout my home, so I finally set up an minidlna server.
+
+What I came to find was that, for example, several years of home videos that I
+shot with a Sony mini-DVD-R camcorder all appeared as "DVD DCR108". This didn't 
+make browsing them easy.
+
+I've been a *nix systems programmer for about 800 years, and that means that
+even though I was born and raised on C/C++, and I've had to do a lot of Java, PHP,
+and so forth, I turn to perl and CPAN first because
+
+A) I'm old, and
+2) CPAN has everything.
+
+Sure enough, the CPAN library Image::ExifTool could let me set the title of various video
+files to whatever I wanted. What I wanted was the file name, because they were things like
+"North Shore Trip 2004.mp4"
+
+So I whipped up this little script that recursively iterates over one or more
+directories specified on the command line, looking for files ending with this
+regular expression (from the code):
+
+my $pattern = "mpeg|mp4|m4v";
+
+It turns out ExifTool can't modify a true MPEG file (I'm not a media file format expert
+so do not ask me why), so this list is probably not right. It so happens that all
+but a few of my files were some variant of mp4, and I could transcode the couple of
+MPEG files I had to mp4, so this did the job for me.
+
+I really know next to NOTHING about media files. It looks like Image::ExifTool could do this
+to PNGs and Jpegs and all sorts of files. I don't know about that. I just know it worked
+for my use case, and here it is if it will be of use to you.
+
+
---
+++ src/mp4titler.pl
@@ -0,0 +1,97 @@
+#! /usr/bin/perl
+
+use strict;
+
+use Image::ExifTool;
+use Data::Dumper;
+use File::Find;
+use File::Basename;
+
+# Regular expression used to match files
+my $pattern = "mpeg|mp4|m4v";
+
+if ( ( scalar @ARGV ) < 1 ) {
+	print
+"Usage:\n\nmp4titler <dirlist>\n\nYou must specify one or more directories to search for $pattern files.\n\n";
+	print << "EOF";
+The purpose of this script is to use the base name of media files as the Title
+field of the media metadata, adding or replacing as necessary.
+
+It was developed by Michael Schwarz (mschwarz at multitool dot net) to
+easily manage and assign titles to MP4 video files from various devices that
+either do not assign titles or assign meaningless titles.
+
+This recursively iterates over one or more directories given on the command line
+and processes any files matching the regular expression given in the anonymous
+function in the call to find().
+
+It should be able to handle ANY format that the CPAN library Image::ExifTool
+supports.
+
+The variable \$mod determines how many files are processed between activity updates
+on the screen. Set it to 0 to get no progress messages.
+EOF
+	exit 1;
+}
+
+# File match count
+my $fc = 0;
+# Modulo to report progress, report every $mod files
+my $mod = 3;
+
+find(
+	{
+		no_chdir => 1,
+		wanted   => sub {
+			return if ( !/$pattern/i );
+			
+			$fc++;
+			print "Processed $fc files\r" if ($mod && $fc % $mod == 0);
+			select()->flush() if ($mod && $fc % $mod == 0);
+			
+			my ( $title, $a2, $a3, $a4 ) = fileparse($_);
+			$title =~ s/\.[^.]*$//;
+
+			if ( setTitle( $File::Find::name, $title ) ) {
+			}
+			else {
+				print "\nRetitled [$File::Find::name], file # $fc to [$title]\n";
+			}
+		}
+	},
+	@ARGV
+);
+
+print "\nExamined $fc files total.\n";
+
+exit 0;
+
+
+
+# Change the title of $filename to $title. Returns 0 on success.
+sub setTitle {
+	my ( $filename, $title ) = @_;
+
+	my $et   = new Image::ExifTool;
+	$et->Options("LargeFileSupport", 1);
+	my $info = $et->ImageInfo($filename);
+
+	return 1 if ( $info->{Title} eq $title );
+
+	print "\n";
+	print( ( $info->{Title} eq $title ) ? "EQUAL\t" : "Unequal\t" );
+	print "Title b4 [$info->{Title}] after [$title]\n";
+
+	my $rc = $et->SetNewValue( Title => $title );
+	
+	# Whatever we did, we need to write it out.
+	my $result = $et->WriteInfo($filename);
+	
+	if (!$result) {
+		print "\nFile $fc\nError on [$filename] ";
+		print $et->GetValue("Error");
+		print "\n\n";
+	}
+	
+	return !$result;
+}
